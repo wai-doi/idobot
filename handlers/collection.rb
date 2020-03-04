e1b728792800dd38db5380079fbfe1f2c610401d
wai-doi <yusuke.doi@esm.co.jp>
2020-03-04 13:26:49 +0900

    Add pretty collection

--- handlers/collection.rb
+++ handlers/collection.rb
@@ -2,14 +2,31 @@ module Ruboty
   module Handlers
     class Collection < Base
       on(
-        /まとめて\z/i,
+        /まとめて\z/,
         name: "collection",
         description: "今日の自分のメッセージをまとめてくれる！"
       )
 
+      on(
+        /きれいにまとめて\z/,
+        name: "pretty_collection",
+        description: "今日の自分のメッセージをきれいにまとめてくれる！",
+      )
+
       def collection(message)
-        body = BodyGenerator.new(message.original.slice(:from_name, :room_id)).generate_body
-        message.reply(body)
+        attributes = message.original.slice(:from_name, :room_id)
+        message.reply(body(attributes))
+      end
+
+      def pretty_collection(message)
+        attributes = message.original.slice(:from_name, :room_id).merge(pretty: true)
+        message.reply(body(attributes))
+      end
+
+      private
+
+      def body(attributes)
+        BodyGenerator.new(attributes).generate_body
       end
     end
   end
--- handlers/collection/body_generator.rb
+++ handlers/collection/body_generator.rb
@@ -5,16 +5,17 @@ module Ruboty
         API_URL = 'https://idobata.io/api/messages'
         CURL_LIMIT = 10
 
-        def initialize(from_name:, room_id:)
+        def initialize(from_name:, room_id:, pretty: false)
           @from_name = from_name
           @room_id = room_id
+          @pretty = pretty
         end
 
         def generate_body
           messages_from_api
             .select { |m| m.sent_by?(@from_name) && !m.mention_to_bot? && m.sent_on_today? }
             .map(&:body_plain)
-            .join("\n")
+            .join(join_string)
         end
 
         private
@@ -36,6 +37,10 @@ module Ruboty
           query += "&older_than=#{older_id}" if older_id
 
           `curl "#{API_URL}#{query}" -H "X-API-Token: #{ENV['IDOBATA_API_TOKEN']}"`
+        end
+
+        def join_string
+          @pretty ? "\n\n---\n" : "\n"
         end
       end
     end
